80714a2231bd15cb065779584ad5511e04f626a8
Mustafa Ayaz <115315223+MustafaAyaz713@users.noreply.github.com>
2025-06-12 13:11:37 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@ using MiniStokTakipAPI.Data; // DbContext için
 
 var builder = WebApplication.CreateBuilder(args);
 
-// EF Core + MySQL bağlantısı
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(9, 3, 0))));
 
-// ✨ Eksik olan burası:
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -24,7 +23,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// ✨ Eksik olan burası:
+
 app.MapControllers();
 
 app.Run();
